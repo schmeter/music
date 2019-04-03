8dfb29ddbbea8c4e6d85c7a7e69ad9dddee5f6d9
Peter Schmidt <peter.schmidt@xing.com>
2019-04-03 17:54:33 +0200

    refactor: Link component

--- src/js/components/Link/Link.js
+++ src/js/components/Link/Link.js
@@ -1,45 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
-import autobind from 'autobind-decorator';
 
 
 const isExternal = (path) => /^https?:\/\/.+/.test(path);
-const isEmpty = (path) => (!path || path === '#');
-const isExternalOrEmpty = (path) => isExternal(path) || isEmpty(path);
 
 class Link extends React.Component {
-    @autobind
-    handleClickLink(e) {
-        const { to, onClick } = this.props;
-        onClick && onClick(e);
-        if (isExternalOrEmpty(to)) {
-            !e.defaultPrevented && e.preventDefault();
-            if (isExternal(to)) {
-                // new window for full paths
-                window.open(to);
-            }
-        }
-    }
-
     render() {
-        const { className, to, children } = this.props;
-        return (
+        const { className, to, onClick, children } = this.props;
+        const props = {
+            className,
+            onClick
+        };
+        const content = children || to;
+        return isExternal(to) ? (
+            <a
+                target="_blank"
+                href={to}
+                {...props}
+            >
+                {content}
+            </a>
+        ) : (
             <NavLink
                 exact
-                className={className}
-                activeClassName={isExternalOrEmpty(to) ? '' : 'active'}
-                to={to || ''}
-                onClick={this.handleClickLink}
+                activeClassName="active"
+                to={to}
+                {...props}
             >
-                {children || to}
+                {content}
             </NavLink>
         );
     }
 }
 
 Link.propTypes = {
-    to: PropTypes.string,
+    to: PropTypes.string.isRequired,
     children: PropTypes.node,
     onClick: PropTypes.func,
     className: PropTypes.string
