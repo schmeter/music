31eae65ef9f66a40629e222b5f4c6c19bc7c8f34
Peter Schmidt <peter.schmidt@xing.com>
2019-04-05 14:35:30 +0200

    test: fix ScrollTop tests and snapshots

--- src/js/components/ScrollTop/ScrollTop.test.js
+++ src/js/components/ScrollTop/ScrollTop.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Router, MemoryRouter } from 'react-router';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 
 import ScrollTop from './ScrollTop';
@@ -17,43 +16,24 @@ describe('ScrollTop', () => {
 
     it('renders the children', () => {
         const target = '.foo';
-        const location = { pathname: 'bar' };
         const component = shallow(
-            <ScrollTop target={target} location={location}>test</ScrollTop>
+            <ScrollTop target={target}>
+                test
+            </ScrollTop>
         );
         expect(toJson(component)).toMatchSnapshot();
     });
 
-    it('scrolls to top on route-change', () => {
-        const target = '.foobar';
-        const component = mount(
-            <MemoryRouter initialEntries={['/']}>
-                <ScrollTop target={target}>
-                    test
-                </ScrollTop>
-            </MemoryRouter>
-        );
-        const router = component.find(Router);
-        const { history } = router.props('history');
-
-        history.push('/lorem');
-
-        expect(scrollTop).toHaveBeenCalledWith(target);
-    });
-
     it('scrolls to top on re-render', () => {
         const target = '.ipsum';
-        const location = { pathname: 'bar' };
-        const component = mount(
-            <MemoryRouter>
-                <ScrollTop target={target} location={location} onReRender>
-                    test
-                </ScrollTop>
-            </MemoryRouter>
+        const component = shallow(
+            <ScrollTop target={target}>
+                test
+            </ScrollTop>
         );
 
         component.setProps({
-            location: 'foo'
+            children: 'foo'
         });
 
         expect(scrollTop).toHaveBeenCalledWith(target);
