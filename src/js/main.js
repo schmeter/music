480be270b7e3097d02614483b687999caba2bcb8
Peter Schmidt <peter.schmidt@xing.com>
2019-08-01 17:00:45 +0200

    refactor: use store file

--- src/js/main.js
+++ src/js/main.js
@@ -3,26 +3,10 @@ import 'whatwg-fetch';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 
-import { initialState as audio } from './state/reducers/audio';
-import { initialState as auth } from './state/reducers/auth';
-import { initialState as layer } from './state/reducers/layer';
+import store from './store';
 import App from './components/App';
-import rootReducer from './state/reducers';
-import middlewares from './state/middlewares';
-
-const store = createStore(
-    rootReducer,
-    {
-        audio,
-        auth,
-        layer
-    },
-    composeWithDevTools(applyMiddleware(...middlewares()))
-);
 
 ReactDOM.render(
     <Provider store={store}>
---
+++ src/js/store.js
@@ -0,0 +1,18 @@
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+
+import { initialState as audio } from './state/reducers/audio';
+import { initialState as auth } from './state/reducers/auth';
+import { initialState as layer } from './state/reducers/layer';
+import rootReducer from './state/reducers';
+import middlewares from './state/middlewares';
+
+export default createStore(
+    rootReducer,
+    {
+        audio,
+        auth,
+        layer
+    },
+    composeWithDevTools(applyMiddleware(...middlewares()))
+);
